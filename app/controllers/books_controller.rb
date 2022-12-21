87b5312d2c20917bc0a349040354be8c56c12995
EC2 Default User <ec2-user@ip-172-31-45-76.ap-northeast-1.compute.internal>
2022-12-21 11:31:19 +0000

    [Add]応用課題2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-   before_action :ensure_correct_user, only: [:update,:edit]
+   before_action :ensure_correct_user, only: [:update,:edit,:destroy]
 
 
   def show
