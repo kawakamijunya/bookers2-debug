6adb9fb5c00c035deae9825bfadf164bee52b3ca
EC2 Default User <ec2-user@ip-172-31-45-76.ap-northeast-1.compute.internal>
2023-01-05 08:51:04 +0000

    [Add]応用課題8aコントローラー

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -3,6 +3,4 @@ class Chat < ApplicationRecord
   belongs_to :user
   belongs_to :room
 
-  validates :message, presence: true
-
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
 
   has_many :users, through: :user_rooms
-  has_many :user_rooms, dependent: :destroy
+  has_many :user_rooms, dependent: :destroy #1つのルームにいるユーザ数は2人のためhas_manyになる
   has_many :chats, dependent: :destroy
 
 end
