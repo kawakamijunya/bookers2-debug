3045267df4832f12191fea97d02c4e9778e21be6
EC2 Default User <ec2-user@ip-172-31-45-76.ap-northeast-1.compute.internal>
2023-01-07 08:23:35 +0000

    [Add]応用課題8aDM機能

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,22 +2,25 @@ class ChatsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = User.find(params[:id])
-    rooms = current_user.user_rooms.pluck(:room_id)
-    user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
+    @user = User.find(params[:id]) #①チャット相手の特定
+    rooms = current_user.user_rooms.pluck(:room_id) #②自分(current_user)に紐付くRoomを全て取得
+    user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms) #③チャット相手と共通のRoomを持つ中間テーブルがあるか確認
 
-    if user_rooms.nil?
+    if user_rooms.nil? #⑥共通のチャットルームがない場合
+      #⑥-1 新しくチャットルームを作る
       @room = Room.new
       @room.save
+      #⑥-2 そのルームidを共通してもつ中間テーブルを、相手と自分の2人分作る
       UserRoom.create(user_id: @user.id, room_id: @room.id)
       UserRoom.create(user_id: current_user.id, room_id: @room.id)
     else
-      @room = user_rooms.room
+      @room = user_rooms.room #④共通のチャットルームがあれば、それに紐付くroomを「@room」に代入する
     end
-
+    #⑤「チャット履歴(@chats)の取得」「新規投稿用の空インスタンス(@chat)作成」
     @chats = @room.chats
-    @chat.save
+    @chat = Chat.new(room_id: @room.id)
   end
+
 
   def create
     @chat = current_user.chats.new(chat_params)
@@ -29,5 +32,10 @@ class ChatsController < ApplicationController
   def chat_params
     params.require(:chat).permit(:message,:room_id)
   end
+
+  def chat_params
+    params.require(:chat).permit(:message, :room_id)
+  end
+
 
 end
